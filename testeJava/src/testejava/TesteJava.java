b243d0b71e6619a0f7b362e0ef83a5e14c0246e5
champiao <39089594+champiao@users.noreply.github.com>
2020-09-21 19:40:59 -0300

    .
    .

---
+++ testeJava/src/testejava/TesteJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testejava;
+
+/**
+ *
+ * @author Rafael Assis
+ */
+public class TesteJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Frm_Tela_Inicial().setVisible(true);
+    }
+    
+}
